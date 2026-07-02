97d2f3fafa55fed1e31602a40cb4f3efb5c3d3e6
Wade <wanghuimvp@ive.com>
2026-07-02 21:13:37 +0800

    fix(tui): use ascii axon wordmark

--- packages/tui/src/logo.ts
+++ packages/tui/src/logo.ts
@@ -1,9 +1,9 @@
 export const logo = String.raw`
- █████  ██   ██  ██████  ███    ██
-██   ██  ██ ██  ██    ██ ████   ██
-███████   ███   ██    ██ ██ ██  ██
-██   ██  ██ ██  ██    ██ ██  ██ ██
-██   ██ ██   ██  ██████  ██   ████
+   A    X   X   OOO   N   N
+  A A    X X   O   O  NN  N
+ AAAAA    X    O   O  N N N
+ A   A   X X   O   O  N  NN
+ A   A  X   X   OOO   N   N
 `.trim()
 
 export const go = {
